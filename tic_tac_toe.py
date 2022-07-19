888c061be20e40d2a37351df1dfe29a3b0d41f00
Izuagie Praise <41047955+Izugance@users.noreply.github.com>
2022-07-19 13:59:14 +0100

    Enforce PEP-8 standards

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -32,15 +32,16 @@ Position = tuple[int, int]
 class Board:
     """Representation of a 3-by-3 Tic-Tac-Toe board."""
 
-    _AXES = ({(0, 0), (0, 1), (0, 2)},   # Rows.
-            {(1, 0), (1, 1), (1, 2)},
-            {(2, 0), (2, 1), (2, 2)},
-            {(0, 0), (1, 0), (2, 0)},   # Columns.
-            {(0, 1), (1, 1), (2, 1)},
-            {(0, 2), (1, 2), (2, 2)},
-            {(0, 0), (1, 1), (2, 2)},   # Diagonals.
-            {(2, 0), (1, 1), (0, 2)}
-            ) 
+    _AXES = (
+        {(0, 0), (0, 1), (0, 2)},  # Rows.
+        {(1, 0), (1, 1), (1, 2)},
+        {(2, 0), (2, 1), (2, 2)},
+        {(0, 0), (1, 0), (2, 0)},  # Columns.
+        {(0, 1), (1, 1), (2, 1)},
+        {(0, 2), (1, 2), (2, 2)},
+        {(0, 0), (1, 1), (2, 2)},  # Diagonals.
+        {(2, 0), (1, 1), (0, 2)},
+    )
 
     def __init__(self, players: Players) -> None:
         """Initialize a Tic-Tac-Toe board."""
@@ -57,32 +58,32 @@ class Board:
         """
         relevant_axes = (axis for axis in Board._AXES if pos in axis)
         for axis in relevant_axes:
-            vals = list(filter(lambda x: self._grid[x[0]][x[1]] != ' ', axis))
+            vals = list(filter(lambda x: self._grid[x[0]][x[1]] != " ", axis))
             if len(vals) < 3:
-                continue                # Row isn't filled.
+                continue  # Row isn't filled.
             else:
                 if len(set(self._grid[row][col] for row, col in vals)) > 1:
-                    continue            # Row contains different values.
+                    continue  # Row contains different values.
                 return True
-            
+
         return False
 
     def reset_board(self) -> None:
         """Clear the board and reset count statistics."""
-        self._grid = [[' '] * 3 for i in range(3)]
+        self._grid = [[" "] * 3 for i in range(3)]
         self._turn = 0
-        self._filled_squares = 0  
+        self._filled_squares = 0
 
     def display_board(self) -> None:
         """Display the board with its values."""
         for i in range(3):
             print(
-                self._grid[i][0], '|', self._grid[i][1], '|',
-                self._grid[i][2], sep='')
+                self._grid[i][0], "|", self._grid[i][1], "|", self._grid[i][2], sep=""
+            )
             if i < 2:
-                print('-', '+', '-', '+', '-', sep='')
-        print('\n')
-        
+                print("-", "+", "-", "+", "-", sep="")
+        print("\n")
+
     def move(self, pos: Position) -> None:
         """Update the board with a player's move at a position."""
         row, col = pos
@@ -97,17 +98,24 @@ class Board:
                 # The player with the most recent turn wins.
                 raise Win(f"Player {self._players[self._turn]!r} wins!")
         if self._filled_squares == 9:
-            raise GameOver('Game Over!')
-        self._turn = int(not self._turn)    # Easy here since we're using 1 or 0.
-            
+            raise GameOver("Game Over!")
+        self._turn = int(not self._turn)  # Easy here since we're using 1 or 0.
+
 
 class TicTacToe:
     """3-by-3 grid tic-tac-toe game class."""
-    
-    ALLOWED_POSITIONS = {'1': (0, 0), '2': (0, 1), '3': (0, 2),
-                         '4': (1, 0), '5': (1, 1), '6': (1, 2),
-                         '7': (2, 0), '8': (2, 1), '9': (2, 2)
-                        }
+
+    ALLOWED_POSITIONS = {
+        "1": (0, 0),
+        "2": (0, 1),
+        "3": (0, 2),
+        "4": (1, 0),
+        "5": (1, 1),
+        "6": (1, 2),
+        "7": (2, 0),
+        "8": (2, 1),
+        "9": (2, 2),
+    }
 
     def __init__(self) -> None:
         """Create a TicTacToe instance."""
@@ -115,18 +123,18 @@ class TicTacToe:
 
     def _display_exception(self, ex: BaseException) -> None:
         print(str(ex))
-        print('\n')
+        print("\n")
 
     def _query_replay(self, rematch: bool = False) -> bool:
         query = "Play again? [y/n]: " if not rematch else "Rematch? [y/n]: "
         while True:
             play_again = input(query).lower().strip()
-            if play_again == 'y':
-                print('\n')
+            if play_again == "y":
+                print("\n")
                 if rematch:
                     self._board.reset_board()
                 return True
-            elif play_again in ('n', 'quit'):
+            elif play_again in ("n", "quit"):
                 print("GOODBYE!")
                 return False
 
@@ -136,7 +144,7 @@ class TicTacToe:
         i = 1
         while i <= 2:
             player = input(f"Enter mark (player {i}): ")
-            if player == 'quit':
+            if player == "quit":
                 raise UserExit()
             if len(player) != 1:
                 print("Enter a mark with one value\n")
@@ -145,16 +153,16 @@ class TicTacToe:
             i += 1
 
         return cast(Players, tuple(players))
-                 
+
     def _get_player_move(self) -> Position:
         """Get a valid move from a player."""
         while True:
             try:
                 print(f"Player {self._board._turn + 1}")
                 in_ = input("Enter position: ").lower().strip()
-                if in_ == 'quit':
+                if in_ == "quit":
                     raise UserExit()
-                elif in_ == 'ff':
+                elif in_ == "ff":
                     raise Forfeit()
                 pos = self._validate_move(in_)
             except InvalidPosition as ex:
@@ -166,11 +174,11 @@ class TicTacToe:
     def _validate_move(self, pos: str) -> Position:
         """Validate a player's move."""
         if not pos in self.ALLOWED_POSITIONS:
-            raise InvalidPosition('Position does not exist')
-        
+            raise InvalidPosition("Position does not exist")
+
         row, col = self.ALLOWED_POSITIONS[pos]
-        if self._board._grid[row][col] != ' ':
-            raise InvalidPosition('Position is occupied')
+        if self._board._grid[row][col] != " ":
+            raise InvalidPosition("Position is occupied")
 
         return row, col
 
@@ -190,7 +198,7 @@ class TicTacToe:
             "Enter 'quit' at any point in the game to exit.\n"
         )
         print(layout)
-        
+
     def main(self):
         """TicTacToe's main game loop."""
         self.display_intro()
@@ -218,5 +226,5 @@ class TicTacToe:
                         raise UserExit()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     TicTacToe().main()
